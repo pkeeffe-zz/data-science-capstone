b4cb42e5a8df401a1a4ac1a83546e6584f5db4e0
pkeeffe@eircom.net <mailto@pkeeffe@eircom.net>
2018-04-30 22:23:26 +0100

    Initial commot

---
+++ main.R
@@ -0,0 +1,7 @@
+library(corpus)
+library(Caret)
+
+twitter <- readLines('data/en_US/en_US.twitter.txt')
+news <- readLines('data/en_US/en_US.news.txt')
+blogs <- readLines('data/en_US/en_US.blogs.txt')
+blogs <- readLines('data/en_US/en_US.blogs.txt')
