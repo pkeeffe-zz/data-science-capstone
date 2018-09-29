0a0836e5dc42b41e5af7281fba581c9d6a771755
pokeeffe2 <patrick_okeeffe@apple.com>
2018-09-29 17:18:28 +0100

    updates

--- main.R
+++ main.R
@@ -2,12 +2,68 @@ library(corpus)
 library(caret)
 library(gsubfn)
 library(stopwords)
+library(ngram)
 
-setwd('d:/dev/coursera/capstone/nlp project')
+setwd('~/data-science-capstone/')
 
 twitter <- readLines('data/en_US/en_US.twitter.txt')
 news <- readLines('data/en_US/en_US.news.txt')
 blogs <- readLines('data/en_US/en_US.blogs.txt')
+
+blogs.3gram <- term_stats(blogs,ngrams=3,filter = text_filter(drop_punct=TRUE))
+
+blogs.sentences <- text_split(blogs,units = "sentence")
+
+ng <- ngram(blogs,n = 2, sep = " ")
+
+blogs.ts.3gram <- term_stats(blogs.sentences,ngrams=3,filter = text_filter(drop_punct=TRUE))
+blogs.ts.2gram <- term_stats(blogs.sentences,ngrams=3,filter = text_filter(drop_punct=TRUE))
+blogs.ts.1gram <- term_stats(blogs.sentences,ngrams=3,filter = text_filter(drop_punct=TRUE))
+
+term_matrix(blogs.sentences,ngrams=1:3,filter = text_filter(drop_punct=TRUE))
+
+
+news.words <- text_split(news,units = 'token')
+term_stats(news,ngrams=1,filter = text_filter(drop_punct=TRUE))
+
+blogs.sentences.freq <- text_ntoken(blogs.sentences)
+blogs.sentences.freq <- blogs.sentences.freq[!(blogs.sentences.freq>100)]  
+blogs.sentences.freq <- blogs.sentences.freq[!(blogs.sentences.freq1<3)]  
+gdf <- as.data.frame(blogs.sentences.freq1)
+g <- ggplot (gdf,aes(x=gdf$blogs.sentences.freq1)) + 
+    geom_histogram(aes(y=..density..), binwidth = 0.5) +
+    geom_density(alpha = .2, fill = '#FF6666') +
+    labs(x='Sentence Length',y='Dist. Density') +
+    ggtitle('Distribution of Blog Sentence Length')
+g
+
+plot(d)
+max(blogs.sentences.freq)
+sum(blogs.sentences.freq1>100)
+
+
+
+
+head(blogs.sentences.freq1)
+
+sum(text_nsentence(blogs.sentences))
+
+
+text <- c("I saw Mr. Jones today.", 
+          "Split across\na line.",
+          "What. Are. You. Doing????",
+          "She asked 'do you really mean that?' and I said 'yes.'")
+
+# split text into sentences
+text_sen <- text_split(text, units = "sentences")
+text.sentences.freq <- text_ntoken(text_sen)
+  
+# get the number of sentences
+text_sen <- text_nsentence(text)
+
+
+
+
 
 blogs_cp <- scan(file='data/en_US/en_US.blogs.txt',sep='\n',what='char',quote='')
 blogs_cp <- tolower(blogs_cp)
@@ -19,6 +75,15 @@ stop.list <- c('the','a','of','and','is','be','are')
 words.vector2 <- words.vector[!words.vector%in%stop.list]
 freq.list <- table(words.vector2)
 sorted.freq.list <- sort(freq.list,decreasing = T)
+word.df <- as.data.frame(sorted.freq.list[1:20])
+require(scales)
+g <- ggplot (word.df, aes(x=words.vector2,y=Freq)) + 
+  geom_bar(stat='identity') + 
+  scale_y_continuous(labels = comma)
+g
+
+
+news_cp <- scan(file='data/en_US/en_US.news.txt',sep='.',what='char',quote='')
 
 #remove hapax legomena (words that occur only once in the corpora)
 sorted.freq.list <- sorted.freq.list[sorted.freq.list>1] 
---
+++ prediction-model.R
@@ -0,0 +1,106 @@
+library(tm)
+library(NLP)
+library(RWeka)
+library(stringr)
+
+BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
+TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
+FullTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 4))
+
+#file_src <- DirSource (directory = '~/data-science-capstone/data/en_US/')
+file_src <- DirSource (directory = '~/data-science-capstone/data-min/')
+
+corp <- VCorpus(file_src,readerControl = list(
+  reader = readPlain,
+  language = 'en_US',
+  load = 'false',
+  dbcontrol = list (
+    useDb = TRUE,
+    dbName = 'texts.db',
+    dbType = 'DB1'
+  )
+))
+
+corp <- tm_map(corp,removePunctuation)
+corp <- tm_map(corp,stripWhitespace)
+corp <- tm_map(corp,removeNumbers)
+
+uni_dtm <- DocumentTermMatrix(corp, control =list(
+  tolower = TRUE
+))
+
+bi_dtm <- DocumentTermMatrix(corp, control =list(
+  tokenize = FullTokenizer,
+  tolower = TRUE,
+  weighting = function(x) weightTf(x)
+))
+
+row_vals <- trimws(bi_dtm$dimnames$Terms[grepl('^[A-Za-z]',bi_dtm$dimnames$Terms)])
+col_vals <-  trimws(uni_dtm$dimnames$Terms[grepl('^[A-Za-z]',uni_dtm$dimnames$Terms)])
+  
+t <- matrix(NA,ncol=length(col_vals), nrow = length(row_vals))
+
+dimnames(t) <- list(row_vals,col_vals)
+
+two_gram_count <- sum(str_count(rownames(t), "\\w+") == 2)
+three_gram_count <- sum(str_count(rownames(t), "\\w+") == 3)
+four_gram_count <- sum(str_count(rownames(t), "\\w+") == 4)
+
+i <- 1;
+j <- 1;
+
+i_len <- length(rownames(t))
+j_len <- length(colnames(t))   
+
+for (i in c(1:i_len)) {
+  for (j in c(1:j_len)){
+    cur_sentence <- trimws(paste (rownames(t)[i],colnames(t)[j]))
+    if (cur_sentence %in% row_vals) {
+      if (str_count(cur_sentence, "\\w+") == 2) {
+        t[i,j] <- sum(bi_dtm[1:3,cur_sentence])/two_gram_count
+      } else if (str_count(cur_sentence, "\\w+") == 3) {
+        t[i,j] <- sum(bi_dtm[1:3,cur_sentence])/three_gram_count
+      } else {
+        t[i,j] <- sum(bi_dtm[1:3,cur_sentence])/four_gram_count
+      }
+    }
+  }
+}
+
+write.csv2(t,file='~/t2.csv')
+
+#Clean up the term document matrices
+bi_dtmm <- as.matrix(bi_dtm)
+
+#Calculate the probability across all 3 documents in the Corpus.
+bi_dtmm <- cbind(bi_dtmm,rowSums(bi_dtmm)/nrow(bi_dtmm))
+colnames(bi_dtmm)[4] <- 'Prob'
+
+bigram_prob <- bi_dtmm[,'Prob']
+ttrigram_prob <- tri_dtmm[,'Prob']
+
+bigram_prob <- bigram_prob[str_count(names(bigram_prob), '\\w+') > 1]
+trigram_prob <- trigram_prob[str_count(names(bigram_prob), '\\w+') > 2]
+startstates <- unique(str_extract(names(bigram_prob), "([A-Za-z]+)"))
+
+
+# Build a 3 dimensional matrix to represent probablility of each n-gram.
+matrix_elements <- 
+
+test <- "we"
+
+if (str_count(test, '\\w+') == 1) {
+  nextstates <- grep('^' + test + ' ',names(bigram_prob), value = T)
+  recommended <- names(which(dtmm[nextstates,4] == max(dtmm[nextstates,4])))  
+  } else if (str_count(test, '\\w+') == 2) {
+  
+}
+
+nextstates <- grep('^we ',names(bigram_prob), value = T)
+dtmm[nextstates,4]
+
+
+
+
+corp_clean <- tm_map(corp,stripWhitespace)
+corp_clean <- tm_map(corp,tolower)
